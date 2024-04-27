07ef66949e75f961c9ff23175194eef1deb4155b
chris abbott <chris.abs96@gmail.com>
2024-04-28 00:29:51 +0100

    chore: updated main menu component to correctly render open sections

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -17,19 +17,71 @@ export default function MainMenu() {
           }`}
         />
 
-        <nav
-          className={`${
-            isOpen
-              ? ''
-              : 'block z-10 h-screen w-screen -translate-y-[31%] transition duration-500 ease-in-out'
-          }`}>
-          <div id='nav-links' className='mt-3 flex gap-2 p-3'>
-            <a
-              className='nav-link cursor m-0 text-xl uppercase text-muted-foreground no-underline'
-              href='/'>
-              <h2 className='nav-link-label'>Home</h2>
-              <div className='nav-link-image mt-3 aspect-[1.8/1] h-full w-1/5 min-w-[400px] rounded-md bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover' />
-            </a>
+        <nav className='mx-6 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+          <div
+            className={`${
+              isOpen
+                ? ''
+                : ' -translate-y-[130%] transition duration-500 ease-in-out'
+            }`}>
+            <div className='flex gap-3'>
+              <a className='m-0 w-full text-xl uppercase text-muted-foreground no-underline'>
+                <h2>Title 1</h2>
+                <div className='mt-3 aspect-[1.8/1] h-[320px] w-full rounded-md bg-red-400 bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover'></div>
+              </a>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen
+                ? ''
+                : ' -translate-y-[130%] transition duration-500 ease-in-out'
+            }`}>
+            <div className='flex gap-3'>
+              <a className='m-0 w-full text-xl uppercase text-muted-foreground no-underline'>
+                <h2>Title 1</h2>
+                <div className='mt-3 aspect-[1.8/1] h-[320px] w-full rounded-md bg-red-400 bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover'></div>
+              </a>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen
+                ? ''
+                : ' -translate-y-[130%] transition duration-500 ease-in-out'
+            }`}>
+            <div className='flex gap-3'>
+              <a className='m-0 w-full text-xl uppercase text-muted-foreground no-underline'>
+                <h2>Title 1</h2>
+                <div className='mt-3 aspect-[1.8/1] h-[320px] w-full rounded-md bg-red-400 bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover'></div>
+              </a>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen
+                ? ''
+                : ' -translate-y-[130%] transition duration-500 ease-in-out'
+            }`}>
+            <div className='flex gap-3'>
+              <a className='m-0 w-full text-xl uppercase text-muted-foreground no-underline'>
+                <h2>Title 1</h2>
+                <div className='mt-3 aspect-[1.8/1] h-[320px] w-full rounded-md bg-red-400 bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover'></div>
+              </a>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen
+                ? ''
+                : ' -translate-y-[130%] transition duration-500 ease-in-out'
+            }`}>
+            <div className='flex gap-3'>
+              <a className='m-0 w-full text-xl uppercase text-muted-foreground no-underline'>
+                <h2>Title 1</h2>
+                <div className='mt-3 aspect-[1.8/1] h-[320px] w-full rounded-md bg-red-400 bg-[url(https://images.unsplash.com/photo-1597382048067-7923725a1524?q=80&w=1523&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center object-cover'></div>
+              </a>
+            </div>
           </div>
         </nav>
 
@@ -37,7 +89,7 @@ export default function MainMenu() {
           id='nav-toggle'
           type='button'
           onClick={toggle}
-          className='z-3 pointer fixed bottom-12 left-1/2 h-20 w-20 translate-x-1/2 rounded-[5rem] border-none bg-white shadow-black outline-none transition-transform duration-300 ease-in hover:scale-125 dark:invert'>
+          className='z-3 pointer fixed bottom-12 left-1/2 h-20 w-20 -translate-x-1/2 rounded-[5rem] border-none bg-white shadow-black outline-none transition-transform duration-300 ease-in hover:scale-125 dark:invert'>
           <i
             className={`open duration-300 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform ease-in hover:scale-125 ${
               isOpen
